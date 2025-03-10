79a473527fa2146a5c34d4910450589bd1e20134
egorpon <ponomarenkoegor36@gmail.com>
2025-03-10 21:09:42 +0200

    add reg,login,account_info,update page

---
+++ mycompanyblog/__init__.py
@@ -0,0 +1,33 @@
+import os
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+
+
+app = Flask(__name__)
+
+
+basedir = os.path.abspath(os.path.dirname(__name__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+app.config["SECRET_KEY"] = 'mysecretkey'
+
+db = SQLAlchemy(app)
+Migrate(app,db)
+
+
+login_manager = LoginManager()
+
+login_manager.init_app(app)
+login_manager.login_view = 'user.login'
+
+
+from mycompanyblog.core.views import core
+from mycompanyblog.users.views import users
+from mycompanyblog.error_pages.handlers import error_pages
+app.register_blueprint(core)
+app.register_blueprint(users)
+app.register_blueprint(error_pages)
+
